771d4ff4d2b5db0efa634fc9d93855d06feec75c
Mauricio <ing.mauricioprz@gmail.com>
2020-07-07 14:06:52 -0500

    cambios de imagenes baño

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// export default {
+//      apiKey: "AIzaSyAqravwjvWx3t2YnoMeXLEHsusWGAfn4h0",
+//     authDomain: "usuario-1a36d.firebaseapp.com",
+//     databaseURL: "https://usuario-1a36d.firebaseio.com",
+//     projectId: "usuario-1a36d",
+//     storageBucket: "usuario-1a36d.appspot.com",
+//     messagingSenderId: "299515577039",
+//     appId: "1:299515577039:web:87546ce589ee758b63adba",
+//     measurementId: "G-JESRTEL31C"
+// }
