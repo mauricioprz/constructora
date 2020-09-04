d93518b871c2e2d3291c542766d7ba182859d1fc
Mauricio <ing.mauricioprz@gmail.com>
2020-09-03 21:10:27 -0500

    cambios dia 3 de septiembre

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,14 +2,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyA8W_D8b_8PWxEbBCkeNj-nDxX5o8KKv-o",
-    authDomain: "invie-form-ac243.firebaseapp.com",
-    databaseURL: "https://invie-form-ac243.firebaseio.com",
-    projectId: "invie-form-ac243",
-    storageBucket: "invie-form-ac243.appspot.com",
-    appId: "1:891649795371:web:f06ab8c02a49f4eb02516e",
-    measurementId: "G-8844M38ENF",
-    messagingSenderId: "891649795371"
+    apiKey: "AIzaSyDCIYmja1ZWXF07z0McVEmaNA5xjEOzf98",
+    authDomain: "contact-form-14ce7.firebaseapp.com",
+    databaseURL: "https://contact-form-14ce7.firebaseio.com",
+    projectId: "contact-form-14ce7",
+    storageBucket: "contact-form-14ce7.appspot.com",
+    messagingSenderId: "523593956492",
+    appId: "1:523593956492:web:ddc7da8b223de514818fa9",
+    measurementId: "G-DK4BDNYL5E"
 };
 
 const firebaseConf = firebase.initializeApp(config);
--- src/components/Unidad.js
+++ src/components/Unidad.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Collage0 from "../assets/images/empresa_comprometida/collagehorizontal0.jpg";
 import Collage from "../assets/images/empresa_comprometida/collagehorizontal.jpg";
 import Collage2 from "../assets/images/empresa_comprometida/collagevertical.jpg";
 import "../assets/css/empresa.css";
@@ -9,7 +10,8 @@ export default class Unidad extends Component {
       <div className="background backgroundc">
         <div className="image-collage">
           <div className="collageh">
-          <img width="100%" height="100%" src={Collage} alt="Collage" />
+          <img className="contenedor-marco" width="50%" height="100%" src={Collage} alt="Collage" />
+          <img className="contenedor-marco" width="50%" height="100%" src={Collage0} alt="Collage" />
           </div>
           <div className="collagev">
           <img width="100%" height="100%" src={Collage2} alt="Collage2" />
